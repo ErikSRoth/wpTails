8440c1d61be201c717bc0ac808b6fab3ba446a66
Erik Roth <codeteam@rltechs.com>
2023-09-16 08:49:37 -0600

    add file header

--- configs/gulp.config.js
+++ configs/gulp.config.js
@@ -1,1 +1,18 @@
-//
+/** 
+ * wpTails ©2022-Present Erik Roth <eriksroth@gmail.com>
+ * Licensed under the MIT License <https://opensource.org/licenses/MIT>
+ * SPDX-License-Identifier: MIT
+ * 
+ * Gulp Configuration Settings
+ * @file gulp.config.js
+ * @description Gulp configuration settings for wpTails.
+ * @version 1.0.0
+ * @since 0.1.0
+ * 
+ * @see {@link https://gulpjs.com/docs/en/getting-started/quick-start|Gulp Quick Start}
+ * 
+ * You should NOT need to modify anything in this file. This file contains the
+ * configuration settings for Gulp. If you need to modify the Gulp tasks, you
+ * should do so in the `gulpfile.babel.js` file in the root directory.
+ * 
+ */
