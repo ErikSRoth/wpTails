f3fb93a7cb6d0dd722fd73a7fa842794612a4918
Erik Roth <codeteam@rltechs.com>
2023-09-16 10:13:18 -0600

    remove @see

--- configs/gulp.config.js
+++ configs/gulp.config.js
@@ -9,8 +9,6 @@
  * @version 1.0.0
  * @since 1.0.0
  * 
- * @see {@link https://gulpjs.com/docs/en/getting-started/quick-start|Gulp Quick Start}
- * 
  * You should NOT need to modify anything in this file. This file contains the
  * configuration settings for Gulp. If you need to modify the Gulp tasks, you
  * should do so in the `gulpfile.babel.js` file in the root directory.
