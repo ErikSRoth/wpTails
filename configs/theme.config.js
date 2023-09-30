106129d7bd8f93864065cd5392d3f3e03cf560ac
Erik Roth <codeteam@rltechs.com>
2023-09-30 11:17:09 -0600

    remove duplicated @

--- configs/theme.config.js
+++ configs/theme.config.js
@@ -7,7 +7,7 @@
  * @file gulp.config.js
  * @description Gulp configuration settings for wpTails Gulp workflow.
  * @package wpTails
- * @file wpTails/gulp/theme.config
+ * 
  * @version 1.0.0
  * @since 1.0.0
  * 
