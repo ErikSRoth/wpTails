fc2f9a031480c6611e4a96d3ffb231d84e5d3119
Erik Roth <codeteam@rltechs.com>
2023-09-30 11:24:20 -0600

    add file header and use info

---
+++ configs/banners.config.js
@@ -0,0 +1,30 @@
+/** 
+ * wpTails ©2022-Present Erik Roth <eriksroth@gmail.com>
+ * Licensed under the MIT License <https://opensource.org/licenses/MIT>
+ * SPDX-License-Identifier: MIT
+ * 
+ * File Banner Configuration Settings
+ * @file banners.config.js
+ * @description Banner configuration settings for various wpTails files.
+ * @package wpTails
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ * 
+ * These mic banners are used to provide information about the file, author,
+ * license, and version. They are meant to be modified to fit your theme. 
+ * The banners are added to the files at build time for both the development
+ * and production versions of the theme.
+ * 
+ * Should you not wish to use the banners, you can remove them from the builds
+ * by editing the 'theme.config.js' file and setting the 'useBanners' property
+ * to 'false' (it is true by default).
+ * 
+ */
+
+/* eslint-env es6 */
+'use strict';
+
+/** Internal Dependencies */
+
+/** External Dependencies */
