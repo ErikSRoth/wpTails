3ec776fbce36776934fd6dbacf47e6d0a9dcd7dd
Erik Roth <codeteam@rltechs.com>
2023-09-30 11:49:51 -0600

    add basic function to test with and build onto

---
+++ gulp/php.js
@@ -0,0 +1,46 @@
+/** 
+ * wpTails ©2022-Present Erik Roth <eriksroth@gmail.com>
+ * Licensed under the MIT License <https://opensource.org/licenses/MIT>
+ * SPDX-License-Identifier: MIT
+ * 
+ * Gulp Configuration Settings
+ * @file php.js
+ * @description Gulp configuration settings for wpTails Gulp workflow.
+ * @package wpTails
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ * 
+ * You should NOT need to modify anything in this file. This file contains the
+ * configuration settings for the Gulp php functions. If you need to modify the Gulp
+ * php tasks and their usage, you should do so in the `gulpfile.babel.js` file in the root directory.
+ * 
+ * TODO: add bug reporting and issue information
+ */
+
+/* eslint-env es6 */
+'use strict';
+
+/** Internal Dependencies */
+import * as gConfig from "../configs/gulp.config";
+import * as tData from "../configs/theme.config";
+
+/** External Dependencies */
+
+/** PHP Development */
+export function phpdev() {
+    const srcPath = [ `${gConfig.srcPath}/**/*.php` ];
+    const destPath = `${gConfig.devPath}`;
+    return gulp.src( srcPath )
+        .pipe( gulp.dest( destPath ) );
+};
+
+/** PHP Distribution */
+export function phpdist() {
+    const srcPath = [ `${gConfig.srcPath}/**/*.php` ];
+    const destPath = `${gConfig.distPath}`;
+    return gulp.src( srcPath )
+        .pipe( gulp.dest( destPath ) );
+};
+
+/** EOF */
