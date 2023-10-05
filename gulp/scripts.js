10fc541199299ee71e922aa0a00c9f3bf3bf66b7
Erik Roth <codeteam@rltechs.com>
2023-10-05 00:04:24 -0600

    add header and initial constants

---
+++ gulp/scripts.js
@@ -0,0 +1,33 @@
+/** 
+ * wpTails ©2022-Present Erik Roth <eriksroth@gmail.com>
+ * Licensed under the MIT License <https://opensource.org/licenses/MIT>
+ * SPDX-License-Identifier: MIT
+ * 
+ * Gulp Configuration Settings
+ * @file scripts.js
+ * @description Gulp configuration settings for wpTails Gulp workflow.
+ * @package wpTails
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ * 
+ * You should NOT need to modify anything in this file. This file contains the
+ * configuration settings for the Gulp javascript functions. If you need to modify the Gulp
+ * javascript tasks and their usage, you should do so in the `gulpfile.babel.js` file in the root directory.
+ * 
+ * TODO: add bug reporting and issue information and ....
+ */
+
+/* eslint-env es6 */
+'use strict';
+
+/** Require Gulp or nothing works! */
+import * as gulp from 'gulp';
+
+/** Internal Dependencies */
+import * as gConfig from "../configs/gulp.config";
+import * as tData from "../configs/theme.config";
+
+/** External Dependencies */
+import { phpTopBnr } from "../configs/banners.config";
+
