9bd117c01e4769c191a1aa97ce76c28587ffb9d4
Erik Roth <codeteam@rltechs.com>
2023-10-17 21:24:09 -0600

    modify test cmd to build main js files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,7 @@ export const setupDev = series( cleanDev,
                                 );
 
 
-export const gtest = series( php.phpDev );
+export const gtest = series( scripts.jsBuildMain );
 
 
 /** EOF */
