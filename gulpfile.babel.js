4210483b797429c7c849277f554cecc597b138aa
Erik Roth <codeteam@rltechs.com>
2023-10-26 21:04:58 -0600

    modify and add clean commands

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,14 +6,13 @@
 
 //Require Gulp or nothing works :)
 //const gulp = require('gulp');
-import {parallel, series} from 'gulp';
+import { parallel, series} from 'gulp';
 
 /** Internal Dependencies */
 const gConfig = require( "./configs/gulp.config" );
 const tConfig = require( "./configs/theme.config" );
 
 /** External Dependencies */
-var args = require('yargs').argv;
 import * as clean from './gulp/clean';
 import * as php from './gulp/php';
 import * as styles from './gulp/styles';
@@ -23,24 +22,24 @@ import * as fonts from './gulp/fonts';
 
 /** Gulp Tasks */
 
+/** Gulp Cleanup Tasks */
 /** Wipe Development Build Folders */
-export const cleanDev = parallel( clean.ccdev, clean.cmdev, clean.cgdev );
-
+export const cleanDev = parallel( clean.cmdev );
+/** Wipe Child Development Build Folders */
+export const cleanChild = parallel( clean.ccdev );
 /** Wipe Distribution Build Folders */
 export const cleanDist = parallel( clean.ccdist, clean.cmdist, clean.cgdist );
+/** Wipe GPL Development Build Folders */
+export const cleanGpl = parallel( clean.cgdev);
+/** Wipe ALL Distribution & Development Build Folders*/
+export const cleanAll = parallel( clean.ccdev, clean.cmdev, clean.cgdev, clean.ccdist, clean.cmdist, clean.cgdist );
+/** Wipe CSS Development Folders */
+export const cleanCssDev = parallel( clean.clnCss );
+/** Wipe JS Devvelopment Folder */
+export const cleanJsDev = parallel( clean.clnJs );
+/** End Of Gulp Cleanup Tasks */
 
-/** Gulp Cleanup Tasks */
-function cleanUp( done ) {
 
-    if ( args.all ) {
-       series( clean.ccdev, clean.cmdev, clean.cgdev, clean.ccdist, clean.cmdist, clean.cgdist );
-    } else if ( args.dist ) {
-        series( clean.ccdist, clean.cmdist, clean.cgdist );
-    } else if ( args.dev ) {   
-        series( clean.ccdev, clean.cmdev, clean.cgdev );
-    }
-    done();
-}
 
 
 export const setupDev = series( cleanDev, 
@@ -53,7 +52,7 @@ export const setupDev = series( cleanDev,
                                 );
 
 
-export const gtest = series( cleanUp );
+export const gtest = series( php.phpDev );
 
 
 /** EOF */
